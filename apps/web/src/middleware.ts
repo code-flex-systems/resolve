91009059598d1e92334f46943ce82b6143bb0930
owen f <owenfarthing103@gmail.com>
2025-05-28 09:59:54 -0400

    update middleware matcher

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -21,6 +21,6 @@ export const config = {
 		 *  - static files (_next/static, _next/image)
 		 *  - favicon.ico
 		 */
-		'/((?!api/auth|_next/static|_next/image|favicon.ico).*)',
+		'/((?!api/auth|_next/static|_next/image|favicon.ico|login|signup).*)',
 	],
 };
