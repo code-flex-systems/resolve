04d6dfffbe84863be2ab99777030a0abb0695590
owen f <owenfarthing103@gmail.com>
2025-06-04 16:38:57 -0400

    redirect for admin index page

--- apps/web/src/app/admin/page.tsx
+++ apps/web/src/app/admin/page.tsx
@@ -1,14 +1,7 @@
 'use client';
 
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
+import { redirect } from 'next/navigation';
 
-export default function AdminRedirect() {
-	const router = useRouter();
-
-	useEffect(() => {
-		router.replace('/admin/users');
-	}, [router]);
-
-	return null;
+export default function AdminIndexPage() {
+	redirect('/admin/users');
 }
